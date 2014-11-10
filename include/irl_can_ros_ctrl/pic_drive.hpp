0a86333165cea0ee773cf2e039f4d87b45d7b49c
fferland <git@francoisferland.com>
2014-11-10 17:05:25 -0500

    Prototype for a PICDrive device driver, untested.

---
+++ include/irl_can_ros_ctrl/pic_drive.hpp
@@ -0,0 +1,119 @@
+#ifndef PICDRIVE_HPP
+#define PICDRIVE_HPP
+
+#include "rc_device.hpp"
+#include "rc_device_factory.hpp"
+#include <irl_can_bus/can_robot_device.hpp>
+
+namespace irl_can_ros_ctrl
+{
+    /// \brief A CANRobotDevice for the PICDrive motor driver from IntRoLab.
+    ///
+    /// ROS Parameters (TODO):
+    ///  - joint_name: The name of the controlled joint.
+    ///                Default: "picdrive_joint"
+    ///
+    class PICDrive: public RCDevice 
+    {
+    private:
+        // CAN-related defines
+        enum
+        {
+            CAN_CMD_SETPOINT                = 0x84,
+            CAN_OUTPUT_DRIVE_STATE1         = 0xFE,
+            ADMITTANCE_K_OFFSET             = 104,
+            ADMITTANCE_D_OFFSET             = 106,
+            ADMITTANCE_I_OFFSET             = 108
+        };
+
+        // ROS parameters
+        std::string joint_name_;
+
+        /// \brief If a new state has been received.
+        bool new_state_;
+
+        /// \brief Conversion factor from the model (rad) to the actuator 
+        /// (pulses).
+        float pos_conv_to_;
+        /// \brief Conversion factor from the actuator (pulses) to the model 
+        /// (rad).
+        float pos_conv_from_;
+        /// \brief Conversion factor from the model (rad/s) to the actuator 
+        /// (pulses/s).
+        float vel_conv_to_;
+        /// \brief Conversion factor from the actuator (pulses/s) to the model 
+        /// (rad/s).
+        float vel_conv_from_;
+        /// \brief Conversion factor from the model (Nm) to the actuator 
+        /// (pulses).
+        float tqe_conv_to_;
+        /// \brief Conversion factor from the actuator (pulses) to the model 
+        /// (Nm).
+        float tqe_conv_from_;
+
+        /// \brief Maximum setpoin in drive units (pulses, torque)
+        /// (pulses, ADC units)
+        int max_setpoint_;
+
+        /// \brief Minimum setpoint in drive units (pulses, torque)
+        /// (pulses, ADC units)
+        int min_setpoint_;
+        
+        /// \brief Reference to the commanded variable. Position by default.
+        double* cmd_var_;
+        /// \brief Reference to the commanded variable conversion ratio.
+        /// Defaults to pos_conv_to_. 
+        float* cmd_conv_to_;
+
+        /// \brief The drive's loop time base, used in speed conversions.
+        float timebase_;
+
+        /// Internal state.
+        double position_;
+        double velocity_;
+        double torque_;
+
+        unsigned short drive_state_; // Drive operation state.
+
+    public:
+        /// \brief Constructor.
+        ///
+        /// Construct a UniDriveV2 in position control mode (default).
+        ///
+        /// \param np Namespace for parameters.
+        PICDrive(const ros::NodeHandle& np); 
+
+        ~PICDrive();
+
+        static RCDevicePtr create(const ros::NodeHandle& np);
+
+        void registerCtrlIfaces(IRLRobot& robot);
+        
+        virtual irl_can_bus::ThrottlingDef 
+            throttled(const irl_can_bus::TimeBase& p) const;
+
+        /// \brief Return the current (CANRobotDevice) state.
+        ///
+        /// Depends on the requested state (req_state_) and if the drive is
+        /// ready to be used.
+        virtual CANRobotDevice::State state() const;
+
+        /// \brief Enable the drive.
+        ///
+        /// Automatically request conversion ratios.
+        /// Switch to enabled state only when all the conversion ratios have
+        /// been received.
+        void enable(irl_can_bus::CANManager& can);
+        
+        void enableCtrl(irl_can_bus::CANManager& can);
+        void disableCtrl(irl_can_bus::CANManager& can);
+        void disable(irl_can_bus::CANManager& can);
+        void requestState(irl_can_bus::CANManager& can);
+        bool stateReady();
+        void processMsg(const irl_can_bus::LaboriusMessage& msg);
+
+    };
+}
+
+#endif
+
---
+++ src/pic_drive.cpp
@@ -0,0 +1,149 @@
+#include <irl_can_ros_ctrl/pic_drive.hpp>
+#include <irl_can_ros_ctrl/irl_robot.hpp>
+#include <irl_can_bus/can_base_macros.h>
+#include <ros/ros.h>
+
+using namespace irl_can_ros_ctrl;
+using namespace irl_can_bus;
+
+namespace {
+    static RCDeviceCreatorHelper ch_("PICDrive", 
+                                     RCDeviceCreator(&PICDrive::create));
+}
+
+PICDrive::PICDrive(const ros::NodeHandle& np): 
+    RCDevice(np),
+    cmd_var_(&position_),
+    cmd_conv_to_(&pos_conv_to_),
+    new_state_(false)
+{
+    np.param("joint_name", joint_name_, std::string("picdrive_joint"));
+
+    double pos_conv_ratio, vel_conv_ratio, tqe_conv_ratio;
+    std::string cmd_var;
+    np.param("position_conv_ratio", pos_conv_ratio, 1.0);
+    np.param("velocity_conv_ratio", vel_conv_ratio, 1.0);
+    np.param("torque_conv_ratio", tqe_conv_ratio, 1.0);
+    np.param("command_variable", cmd_var, std::string("position"));
+
+    pos_conv_to_ = pos_conv_ratio;
+    pos_conv_from_ = 1.0 / pos_conv_ratio;
+    vel_conv_to_ = vel_conv_ratio;
+    vel_conv_from_ = 1.0 / vel_conv_ratio;
+    tqe_conv_to_ = tqe_conv_ratio;
+    tqe_conv_from_ = 1.0 / tqe_conv_ratio;
+
+    // TODO: admittance params
+    // np.param("admittance_conv_to", admit_conv_to_, 64.0);
+    // Converted tu uint16 at update time.
+    //np.param("admittance_k", admit_k_, 0.0);
+    //np.param("admittance_b", admit_b_, 0.0);
+    //np.param("admittance_m", admit_m_, 0.0);
+
+
+    // TODO: We could pick this up from the drive instead.
+    if (cmd_var == "position")
+    {
+        cmd_var_ = &position_;
+        cmd_conv_to_ = &pos_conv_to_;
+    }
+    else if (cmd_var == "velocity")
+    {
+        cmd_var_ = &velocity_;
+        cmd_conv_to_ = &vel_conv_to_;
+    }
+    else if (cmd_var == "torque")
+    {
+        cmd_var_ = &torque_;
+        cmd_conv_to_ = &tqe_conv_to_;
+    }
+}
+
+PICDrive::~PICDrive()
+{
+}
+
+RCDevicePtr PICDrive::create(const ros::NodeHandle& np)
+{
+    return RCDevicePtr(new PICDrive(np));
+}
+
+void PICDrive::registerCtrlIfaces(IRLRobot& robot)
+{
+    hardware_interface::JointStateInterface& jsi = robot.jsi();
+    hardware_interface::JointStateHandle sh(joint_name_, 
+                                            &position_,
+                                            &velocity_, 
+                                            &torque_);
+    jsi.registerHandle(sh);
+
+}
+
+ThrottlingDef PICDrive::throttled(const TimeBase& p) const
+{
+    // No throttling for PICDrive.
+    ThrottlingDef td = {TimeBase(0), 0};
+    return td;
+}
+
+CANRobotDevice::State PICDrive::state() const
+{
+    return CANRobotDevice::state();
+}
+
+void PICDrive::enable(CANManager& can)
+{
+    CANRobotDevice::state(STATE_ENABLED);
+
+    position_      = 0.0;
+    velocity_      = 0.0;
+    torque_        = 0.0;
+}
+
+void PICDrive::enableCtrl(CANManager& can)
+{
+    CANRobotDevice::enableCtrl(can);
+
+    // TODO: Start motor.
+}
+
+void PICDrive::disableCtrl(CANManager& can)
+{
+    CANRobotDevice::disableCtrl(can);
+
+    // TODO: Stop motor.
+}
+
+void PICDrive::disable(CANManager& can)
+{
+    CANRobotDevice::disable(can);
+}
+
+void PICDrive::requestState(CANManager& can)
+{
+    new_state_ = false;
+}
+
+bool PICDrive::stateReady()
+{
+    return new_state_;
+}
+
+void PICDrive::processMsg(const LaboriusMessage& msg)
+{
+    if (msg.msg_type == CAN_TYPE_ACTUATOR_HIGH_PRIORITY &&
+        msg.msg_cmd == CAN_OUTPUT_DRIVE_STATE1) {
+
+        int16_t torque   = *((int16_t*)(&msg.msg_data[0]));
+        int16_t velocity = *((int16_t*)(&msg.msg_data[2]));
+        int32_t position = *((int32_t *)(&msg.msg_data[4]));
+
+        torque_   = torque   * tqe_conv_from_;
+        velocity_ = velocity * vel_conv_from_;
+        position_ = position * pos_conv_from_;
+
+        new_state_ = true;
+    }
+
+}
+
